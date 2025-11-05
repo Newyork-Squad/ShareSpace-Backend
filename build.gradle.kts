1e111e2b4c24c8b17b6d5e9de40d6b6d0989d4ef
abdelrahman <abdelrahmankarim7@gmail.com>
2025-11-05 23:56:11 +0200

    refactor: update the spring doc dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	//swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0") {
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0") {
 		exclude(group = "org.springframework", module = "spring-web")
 		exclude(group = "org.springframework", module = "spring-webmvc")
 	}
