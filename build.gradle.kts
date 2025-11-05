9730877e7477fa5d6797fe0481f81776c8ba68b7
abdelrahman <abdelrahmankarim7@gmail.com>
2025-11-05 23:46:33 +0200

    refactor: update the dependencies to solve blocking dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,10 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	//swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0") {
+		exclude(group = "org.springframework", module = "spring-web")
+		exclude(group = "org.springframework", module = "spring-webmvc")
+	}
 
 
 	//jwt
