6cdcef67579b613ad19022fb0eef83bba872f9b8
abdelrahman <abdelrahmankarim7@gmail.com>
2025-11-17 22:22:40 +0200

    feat : add image file to register calling

--- src/main/kotlin/com/newyork/sharespace/api/controller/AuthController.kt
+++ src/main/kotlin/com/newyork/sharespace/api/controller/AuthController.kt
@@ -13,7 +13,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api/auth")
@@ -23,9 +25,10 @@ class AuthController(
 
     @PostMapping("/register")
     fun register(
-        @Valid @RequestBody body: RegisterRequest
+        @Valid @RequestPart("user") body: RegisterRequest,
+        @RequestPart("file") file: MultipartFile?
     ): ResponseEntity<ApiResponse<User>> {
-        val user = authService.register(body)
+        val user = authService.register(body,file)
 
         val response = ApiResponse.success(
             data = user,
