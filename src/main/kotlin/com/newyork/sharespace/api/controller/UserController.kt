648296c887cb08d0459fc2ad98ca9e19e91ae367
Renad_Alalfy <renadalalfy@gmail.com>
2025-11-20 12:35:33 +0200

    add update account info endpoint

--- src/main/kotlin/com/newyork/sharespace/api/controller/UserController.kt
+++ src/main/kotlin/com/newyork/sharespace/api/controller/UserController.kt
@@ -1,6 +1,7 @@
 package com.newyork.sharespace.api.controller
 
 import com.newyork.sharespace.api.dto.UserResponse
+import com.newyork.sharespace.api.dto.UserUpdateRequest
 import com.newyork.sharespace.api.dto.toUserResponse
 import com.newyork.sharespace.config.JwtAuthFilter
 import com.newyork.sharespace.config.exceptionHandling.ApiResponse
@@ -9,13 +10,7 @@ import com.newyork.sharespace.services.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.server.ResponseStatusException
 
@@ -24,8 +19,8 @@ import org.springframework.web.server.ResponseStatusException
 @CrossOrigin(origins = ["*"])
 class UserController(
     private val userService: UserService,
-    private val userRepository : UserRepository
-    ) {
+    private val userRepository: UserRepository
+) {
 
 
     @GetMapping("/me")
@@ -73,4 +68,26 @@ class UserController(
         )
         return ResponseEntity.ok(response)
     }
+
+    @PostMapping("/me/update")
+    fun updateUser(
+        @RequestBody userUpdateRequest: UserUpdateRequest
+    ): ResponseEntity<ApiResponse<UserResponse>> {
+
+        val userId = JwtAuthFilter.getUserId()
+            ?: return ResponseEntity.status(404)
+                .body(ApiResponse.error("User not found"))
+
+        val updated = userService.updateUserInfo(userId, userUpdateRequest)
+            ?: return ResponseEntity.status(404)
+                .body(ApiResponse.error("User not found"))
+
+        return ResponseEntity.ok(
+            ApiResponse.success(
+                updated.toUserResponse(),
+                "User updated successfully"
+            )
+        )
+    }
+
 }
