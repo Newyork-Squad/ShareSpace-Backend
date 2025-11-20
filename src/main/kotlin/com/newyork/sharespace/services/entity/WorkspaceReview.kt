93023e6a0634ce5839293f0b4ff38e0eaff4f358
Renad_Alalfy <renadalalfy@gmail.com>
2025-11-20 13:54:40 +0200

    add review entity

---
+++ src/main/kotlin/com/newyork/sharespace/services/entity/WorkspaceReview.kt
@@ -0,0 +1,31 @@
+package com.newyork.sharespace.services.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+@Table(name = "workspace_reviews")
+data class WorkspaceReview(
+    @Id
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false)
+    val workspaceId: UUID,
+
+    @Column(nullable = false)
+    val userId: UUID,
+
+    @Column(nullable = false, length = 2000)
+    val comment: String,
+
+    @Column(nullable = false)
+    val rating: Int,
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)
