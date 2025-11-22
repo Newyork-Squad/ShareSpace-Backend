ae41a0b56d983b866a5850c24e8e1f8606d58d86
Renad_Alalfy <renadalalfy@gmail.com>
2025-11-22 15:41:20 +0200

    add find by workspace id and user id function

--- src/main/kotlin/com/newyork/sharespace/repository/WorkspaceReviewRepository.kt
+++ src/main/kotlin/com/newyork/sharespace/repository/WorkspaceReviewRepository.kt
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
 interface WorkspaceReviewRepository : JpaRepository<WorkspaceReview, UUID> {
     fun findAllByWorkspaceIdOrderByCreatedAtDesc(workspaceId: UUID, pageable: Pageable): Page<WorkspaceReview>
+    fun findByWorkspaceIdAndUserId(workspaceId: UUID, userId: UUID): WorkspaceReview?
 }
